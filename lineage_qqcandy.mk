dcf9397c50c8a09af78fb48369ad709ce0778938
wudilsr <wudilsr@gmail.com>
2025-05-14 22:06:05 +0800

    qqcandy: Commonize AOSP target products
    Change-Id: Ida54936d0a2faf8fd9d4c1b16fc252fb084e5fee

--- lineage_qqcandy.mk
+++ lineage_qqcandy.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from the custom device configuration.
 $(call inherit-product, device/oneplus/qqcandy/device.mk)
 
